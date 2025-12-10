85a8e5886bf9785764bfd409ef4f9e99e785b7f6
Eric Aguayo <computergeek90@protonmail.com>
2025-12-09 21:36:11 -0600

    commit 3: testing of mobile responsiveness

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // home page js displaying different event types
 
-
 // Special Events
 // Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you.
 // We’ll be sure to mark your special date with an unforgettable meal.
