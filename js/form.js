894035df138da708f389898fd0b4a15a7392955c
Eric Aguayo <computergeek90@protonmail.com>
2025-12-12 12:40:57 -0600

    third commit: homepage js functionality pushed for review

---
+++ js/form.js
@@ -0,0 +1,31 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form-reservation");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let isValid = true;
+
+    const nameField = document.querySelector(".name-input");
+    const emailField = document.querySelector(".email-input");
+    const dateText = document.querySelector(".date-text");
+    const monthInput = document.querySelector(".input-month");
+    const dayInput = document.querySelector(".input-day");
+    const yearInput = document.querySelector(".input-year");
+    const timeText = document.querySelector(".time-text");
+    const hourInput = document.querySelector(".input-hour");
+    const minuteInput = document.querySelector(".input-minute");
+    const ampmInput = document.querySelector(".input-am-pm");
+    const peopleInput = document.querySelector(".input-people");
+    const subSelection = document.querySelector(".am-pm-submenu");
+    // form submission logic here
+    if (isValid) {
+
+    } else if(isValid) {
+        
+    }
+    else if(isValid) {
+        
+    }
+  });
+});
